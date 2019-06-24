7311bae5ef439f62e3c6d0864b1f33245e475852
miaonagemide <hanzhou244@gmail.com>
2019-06-24 13:36:25 -0700

    Feature: add Horizontal bar chart to overview screen and implement it

--- app/src/main/java/hanzhou/easyledger/ui/OverviewFragment.java
+++ app/src/main/java/hanzhou/easyledger/ui/OverviewFragment.java
@@ -1,5 +1,6 @@
 package hanzhou.easyledger.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,15 +10,80 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import java.util.zip.Inflater;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
 
 import hanzhou.easyledger.R;
 
 public class OverviewFragment extends Fragment {
+    HorizontalBarChart barChart;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       return inflater.inflate(R.layout.fragment_overview,container,false);
+        View root = inflater.inflate(R.layout.fragment_overview,container,false);
+        barChart= root.findViewById(R.id.overview_total_balance_barchart);
+
+        int range = 200;
+
+        setBarChart(2, range);
+
+       return root;
+    }
+
+    private void setBarChart(int i, int range) {
+        ArrayList<BarEntry> entrySpending = new ArrayList<>();
+        ArrayList<BarEntry> entryRevenus = new ArrayList<>();
+        float barWidth = 3f;
+        float spaceForBar = 1f;
+        entryRevenus.add(new BarEntry(spaceForBar, 1450));
+        entrySpending.add(new BarEntry(spaceForBar*2, 750));
+
+        BarDataSet barDataSetSpending = new BarDataSet(entrySpending, "Money out");
+        barDataSetSpending.setColor(getResources().getColor(R.color.color_money_out));
+
+        BarDataSet barDataSetRevenue = new BarDataSet(entryRevenus, "Money in");
+        barDataSetRevenue.setColor(getResources().getColor(R.color.color_money_in));
+
+        BarData data = new BarData();
+        data.addDataSet(barDataSetSpending);
+        data.addDataSet(barDataSetRevenue);
+
+//        data.setBarWidth(barWidth);
+
+        barChart.setData(data);
+        barChart.invalidate();//refresh data
+        barChartSetting();
+
+    }
+
+    private void barChartSetting(){
+        barChartSetStyle();
+        barChartSetNoInteraction();
+
+    }
+
+    private void barChartSetStyle(){
+        barChart.setFitBars(true);
+        barChart.setVisibleYRange(0,1500, YAxis.AxisDependency.LEFT);
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.getAxisLeft().setDrawGridLines(false);
+        barChart.getAxisRight().setDrawGridLines(false);
+    }
+
+    private void barChartSetNoInteraction(){
+        barChart.setTouchEnabled(false);
+        barChart.setDragEnabled(false);
+        barChart.setScaleEnabled(false);
+        barChart.setScaleXEnabled(false);
+        barChart.setScaleYEnabled(false);
+        barChart.setPinchZoom(false);
+        barChart.setDoubleTapToZoomEnabled(false);
+
     }
 }
